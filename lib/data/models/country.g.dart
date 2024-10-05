9614d733783374abdec710f8e3a8adaa27493492
Ankit Kumar <57802410+Ankitkj1999@users.noreply.github.com>
2024-10-06 01:56:59 +0530

    1: graphQL_Inpector.dart 2: repository.dart 3: country.dart 4: cache_manager.dart

---
+++ lib/data/models/country.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'country.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CountryImpl _$$CountryImplFromJson(Map<String, dynamic> json) =>
+    _$CountryImpl(
+      code: json['code'] as String,
+      name: json['name'] as String,
+      emoji: json['emoji'] as String,
+    );
+
+Map<String, dynamic> _$$CountryImplToJson(_$CountryImpl instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'name': instance.name,
+      'emoji': instance.emoji,
+    };
